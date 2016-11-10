8074a35d12da9badc4cdc8bc66da32602ea563c8
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-11-10 18:29:38 +0000

    Implement RecyclerView

---
+++ app/src/main/java/emilywoods/weatherapp/LocationsAdapter.java
@@ -0,0 +1,63 @@
+package emilywoods.weatherapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by emilywoods on 10/11/2016.
+ */
+
+public class LocationsAdapter extends RecyclerView.Adapter<LocationsAdapter.myViewHolder> {
+
+    private List<Locations> locationsList;
+
+    public class myViewHolder extends RecyclerView.ViewHolder {
+        public TextView name, longitude, latitude;
+
+        public myViewHolder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.name);
+            latitude = (TextView) itemView.findViewById(R.id.latitude);
+            longitude = (TextView) itemView.findViewById(R.id.longitude);
+
+        }
+    }
+
+    public LocationsAdapter() {
+        this.locationsList = new ArrayList<>();
+    }
+
+    public void setLocationsList(List<Locations> locationsList) {
+        this.locationsList = locationsList;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public myViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.locations_list, parent, false);
+        return new myViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(myViewHolder holder, int position) {
+        Locations locations = locationsList.get(position);
+        holder.name.setText(locations.getName());
+        holder.name.setText(locations.getLatitude());
+        holder.name.setText(locations.getLatitude());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return locationsList.size();
+    }
+
+
+}
--- app/src/main/java/emilywoods/weatherapp/MainActivity.java
+++ app/src/main/java/emilywoods/weatherapp/MainActivity.java
@@ -3,21 +3,38 @@ package emilywoods.weatherapp;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.ListAdapter;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements WeatherCallback {
 
     private ApiClient apiClient;
+    private RecyclerView recyclerView;
+    private LocationsAdapter lAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = (RecyclerView) findViewById(R.id.location_recycler_view);
+
+        lAdapter = new LocationsAdapter();
+        RecyclerView.LayoutManager lLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(lLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(lAdapter);
+
         apiClient = new ApiClient();
         apiClient.setCallbackListener(this);
+        apiClient.getLocationInfo();
+
+
     }
 
     // onCreate, onPause, onResume, onStart, and onStop
@@ -43,5 +60,6 @@ public class MainActivity extends AppCompatActivity implements WeatherCallback {
     @Override
     public void onLocations(List<Locations> locations) {
         Log.e(getClass().getCanonicalName(), "" + locations.size());
+        lAdapter.setLocationsList(locations);
     }
 }
